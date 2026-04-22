fc34aea40709a28d6214a71821a78e3fc4a9953d
Akhil Shimna Kumar <a.shimnakumar@student.tudelft.nl>
2026-04-22 10:08:06 +0200

    fix: catkin detection instead of ament

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from pathlib import Path
 
 package_name = 'ros2'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=['ros2'],
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch',
+            ['launch/sim.launch.py']),
+        ('share/' + package_name + '/urdf',
+            ['urdf/cuboid_bot.urdf']),
+        ('share/' + package_name + '/worlds',
+            ['worlds/env.world']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='you',
+    maintainer_email='you@example.com',
+    description='Cuboid robot in Gazebo with LiDAR, WASD control, and SLAM-ready setup.',
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'teleop_wasd = ros2.teleop_wasd:main',
